231f8df03e3df7c9a8b776a602edcb37b1a3ec4a
kergeres <55786735+kergeres@users.noreply.github.com>
2020-12-20 20:41:07 +0100

    gallery-mondjuk kész desktop

--- script/main.js
+++ script/main.js
@@ -1,12 +1,10 @@
 let cbOpen = true;
 function myFunction() {
-    // Get the checkbox
+ 
    
    
     if(cbOpen==false)//ha zarva van
     {
-        // document.querySelector(".ham-menu-line").style.display = "block";
-        // document.querySelector("#nav-container").style.display = "none";
         document.querySelector("#nav-container").classList.remove("nav-open");
         document.querySelector(".ham-menu-line").classList.remove("line-rot");
         document.querySelector(".ham-menu-linea").classList.remove("linea-rot");
@@ -16,26 +14,15 @@ function myFunction() {
     }
     else
     {
-        
-        // document.querySelector(".ham-menu-line").style.display = "none";
         document.querySelector("#nav-container").classList.add("nav-open");
         document.querySelector(".ham-menu-line").classList.add("line-rot");
         document.querySelector(".ham-menu-linea").classList.add("linea-rot");
         document.querySelector(".ham-menu-lineb").classList.add("lineb-rot");
-        // document.querySelector("#nav-container").style.display = "block";
+     
         cbOpen = false;
     }
     
-    // if (checkBox.checked == true){
-        
-    //     document.querySelector(".ham-menu-line").style.display = "block";
-    //     document.querySelector("#nav-container").style.display = "block";
     
-    // } else {
-    //     document.querySelector(".ham-menu-line").style.display = "none";
-    //     document.querySelector("#nav-container").style.display = "none";
-        
-    // }
   }
  
  
@@ -57,7 +44,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 2500); 
 } 
 
 
