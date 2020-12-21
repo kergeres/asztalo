24c875e2008e81f1581e6ea46dfc4d0f70e7e306
kergeres <55786735+kergeres@users.noreply.github.com>
2020-12-21 18:46:18 +0100

    finomitas

--- script/main.js
+++ script/main.js
@@ -13,13 +13,22 @@ function hamburgerNav() {
         
         cbOpen = true;
     }
-    else
+    else if(cbOpen==true)
     {
+      
         document.querySelector("#nav-container").classList.add("nav-open");
         document.querySelector(".ham-menu-line").classList.add("line-rot");
         document.querySelector(".ham-menu-linea").classList.add("linea-rot");
         document.querySelector(".ham-menu-lineb").classList.add("lineb-rot");
-     
+
+
+        document.querySelector(".menu-point-cont").addEventListener('click', function() {
+          document.querySelector("#nav-container").classList.remove("nav-open");
+          document.querySelector(".ham-menu-line").classList.remove("line-rot");
+          document.querySelector(".ham-menu-linea").classList.remove("linea-rot");
+          document.querySelector(".ham-menu-lineb").classList.remove("lineb-rot");
+          cbOpen = true;
+        });
         cbOpen = false;
     }
     
